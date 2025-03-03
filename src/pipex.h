c590daceb816d42239633c4447a8c217520ab08e
Zacharia Ivanov <zivanov@f1r2s3.codam.nl>
2025-03-03 17:06:44 +0100

    added used libraries

--- src/pipex.h
+++ src/pipex.h
@@ -6,10 +6,19 @@
 /*   By: zivanov <marvin@42.fr>                        +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/03/03 10:39:35 by zivanov        #+#    #+#                */
-/*   Updated: 2025/03/03 11:43:25 by zivanov        ########   odam.nl        */
+/*   Updated: 2025/03/03 15:25:32 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <stdio.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include "../libft/libft.h"
+
+#endif
