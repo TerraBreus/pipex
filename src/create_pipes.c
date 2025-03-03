ce9569f9a8ff1790fdab136bb7c23ce4ee7d6f3f
Zacharia Ivanov <zivanov@f1r2s3.codam.nl>
2025-03-03 17:05:03 +0100

    function mallocs an array of pipes

--- src/create_pipes.c
+++ src/create_pipes.c
@@ -6,8 +6,33 @@
 /*   By: zivanov <marvin@42.fr>                        +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/03/03 11:43:17 by zivanov        #+#    #+#                */
-/*   Updated: 2025/03/03 11:43:25 by zivanov        ########   odam.nl        */
+/*   Updated: 2025/03/03 15:44:07 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
 
+int	**create_pipes(int cmd_c)
+{
+	int	**pfd_v;
+	int	i;
+
+	pfd_v = (int **) malloc(sizeof(int *) * (cmd_c - 1));
+	if (pfd_v == NULL)
+	{
+		perror("pfd_v malloc failure");
+		exit(EXIT_FAILURE);
+	}
+	i = -1;
+	while (++i < (cmd_c - 1))
+	{
+		pfd_v[i] = (int *) malloc(sizeof(int) * 2);
+		if (pfd_v[i] == NULL)
+		{
+			perror("pfd_v[i] malloc failure");
+			exit(EXIT_FAILURE);
+		}
+		pipe(pfd_v[i]);
+	}
+	return (pfd_v);
+}
