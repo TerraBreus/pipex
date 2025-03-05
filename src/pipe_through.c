fae752f052fe6504fabbbc8bb9d91eb5f9ea46e4
Zacharia Ivanov <zivanov@f1r7s3.codam.nl>
2025-03-05 15:35:14 +0100

    Fixed if condition to correctly check on errors with dup2

--- src/pipe_through.c
+++ src/pipe_through.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <marvin@42.fr>                        +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/03/03 16:31:04 by zivanov        #+#    #+#                */
-/*   Updated: 2025/03/03 16:51:28 by zivanov        ########   odam.nl        */
+/*   Updated: 2025/03/05 15:32:06 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 void	pipe_through(char *cmdline, int **pfd_v, int i, char **paths)
 {
-	if (dup2(pfd_v[i - 1][0], 0))
-		return (perror("pipe_through -> dup2 pfdv_read"));
+	if (dup2(pfd_v[i - 1][0], 0) == -1)
+	{
+		perror("pipe_through -> dup2 pfdv_read");
+		exit(EXIT_FAILURE);
+	}
 	close(pfd_v[i - 1][0]);
-	if (dup2(pfd_v[i][1], 1))
-		return (perror("pipe_through -> dup2 pfdv_write"));
+	if (dup2(pfd_v[i][1], 1) == -1)
+	{
+		perror("pipe_through -> dup2 pfdv_write");
+		exit(EXIT_FAILURE);
+	}
 	close(pfd_v[i][1]);
 	exec_cmd(paths, cmdline);
 }
