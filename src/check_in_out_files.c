d198bc957c6f4d7f95d27306792e2b9cf9cc9036
Zacharia Ivanov <zivanov@f1r6s7.codam.nl>
2025-03-06 16:09:46 +0100

    function now only checks for writability on outfile if it actually exists

--- src/check_in_out_files.c
+++ src/check_in_out_files.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <marvin@42.fr>                        +#+                    */
 /*                                                    +#+                     */
 /*   Created: 2025/03/06 14:32:01 by zivanov        #+#    #+#                */
-/*   Updated: 2025/03/06 14:42:45 by zivanov        ########   odam.nl        */
+/*   Updated: 2025/03/06 16:02:45 by zivanov        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	check_infile_outfile(char *infile, char *outfile)
 		perror("check_infile");
 		exit(EXIT_FAILURE);
 	}
+	if (access(outfile, F_OK) == -1)
+		return ;
 	if (access(outfile, W_OK) == -1)
 	{
 		perror("check_outfile");
