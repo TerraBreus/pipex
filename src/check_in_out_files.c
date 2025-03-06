2e2cad1c701c9aeb0729dc98354b7f13b02d01bb
Zacharia Ivanov <zivanov@f1r6s7.codam.nl>
2025-03-06 15:55:21 +0100

    Checks for correct permissions and existence

---
+++ src/check_in_out_files.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         ::::::::           */
+/*   check_in_out_files.c                                :+:    :+:           */
+/*                                                      +:+                   */
+/*   By: zivanov <marvin@42.fr>                        +#+                    */
+/*                                                    +#+                     */
+/*   Created: 2025/03/06 14:32:01 by zivanov        #+#    #+#                */
+/*   Updated: 2025/03/06 14:42:45 by zivanov        ########   odam.nl        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	check_infile_outfile(char *infile, char *outfile)
+{
+	if (access(infile, F_OK | R_OK) == -1)
+	{
+		perror("check_infile");
+		exit(EXIT_FAILURE);
+	}
+	if (access(outfile, W_OK) == -1)
+	{
+		perror("check_outfile");
+		exit(EXIT_FAILURE);
+	}
+}
